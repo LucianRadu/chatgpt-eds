b22d9652eb8fab88966c2a658d83753f4ed66781
lradu <lradu@adobe.com>
2025-10-27 16:16:17 +0200

    change aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -184,7 +184,7 @@ function toClassName(name) {
 }
 
 /**
- * Sanitizes a string for use as a js property name.
+ * Sanitizes a string for use as a js property name
  * @param {string} name The unsanitized string
  * @returns {string} The camelCased name
  */
