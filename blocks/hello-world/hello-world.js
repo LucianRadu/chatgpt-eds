1c98e608302eede89677bacd0769fe192444c596
lradu <lradu@adobe.com>
2025-10-27 16:58:11 +0200

    fix linting

--- blocks/hello-world/hello-world.js
+++ blocks/hello-world/hello-world.js
@@ -1,6 +1,7 @@
 export default async function decorate(block, onDataLoaded) {
   block.textContent = 'Content loading...';
   onDataLoaded(async (data) => {
+    // eslint-disable-next-line no-console
     console.log('Data loaded', data);
     block.textContent = 'Hello World';
   });
