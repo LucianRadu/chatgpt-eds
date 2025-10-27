812b64766eef32a22da5df22cc69150a33f395d1
lradu <lradu@adobe.com>
2025-10-27 17:22:08 +0200

    fix

--- blocks/hello-world/hello-world.js
+++ blocks/hello-world/hello-world.js
@@ -1,6 +1,6 @@
 export default async function decorate(block, onDataLoaded) {
   block.textContent = 'Content loading...';
-  onDataLoaded(async (data) => {
+  onDataLoaded.then((data) => {
     // eslint-disable-next-line no-console
     console.log('Data loaded', data);
     block.textContent = 'Hello World';
