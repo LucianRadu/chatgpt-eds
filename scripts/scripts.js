c0957d293130d6bef8ce88a6fa504168d2b55933
lradu <lradu@adobe.com>
2025-10-27 16:13:05 +0200

    retrigger deploy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -135,7 +135,7 @@ async function loadLazy(doc) {
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
+  // load anything that can be postponed to the latest here.
 }
 
 async function loadPage() {
